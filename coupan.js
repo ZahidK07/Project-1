65fff739f32a04e140706dc53b05fb64ba9f990c
Zahid Kazi <94307103+ZahidK07@users.noreply.github.com>
2022-01-19 03:42:04 +0530

    not required here

--- coupan.js
+++
@@ -1,7 +0,0 @@
-function openOffer(){
-    document.getElementById('coupan').style.visibility='visible'
-}
-
-function closeBtn(){
-    document.getElementById('coupan').style.visibility='hidden'
-}
